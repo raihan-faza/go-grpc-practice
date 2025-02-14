de4833e866c72c7eb50d362a7f1809d4bf51ac27
lahh <raihanfazasabiqr@gmail.com>
2025-02-14 23:06:59 +0700

    setup grpc server

--- server/main.go
+++ server/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
 
+	"github.com/raihan-faza/go-grpc-practice/userpb"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	userpb.UnimplementedUserServiceServer
+}
+
+func (s *server) GetUser(userpb.GetUserRequest) userpb.GetUserResponse {
+	return userpb.GetUserResponse{}
+}
+func main() {
+	listener, err := net.Listen("tcp", ":1337")
+	if err != nil {
+		panic(err)
+	}
+	grpcServer := grpc.NewServer()
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
